25b51f416b6acb3c84b0a41506e4e330f918ecaf
Dmytro Petrenko <skiff120@gmail.com>
2024-03-11 01:37:40 +0200

    add trading repeat for user events

--- utils/config.py
+++ utils/config.py
@@ -1,6 +1,12 @@
 import json
 import os
 
+import eth_account
+from eth_account.signers.local import LocalAccount
+
+from hyperliquid.exchange import Exchange
+from hyperliquid.info import Info
+
 def get_config():
     config_path = os.path.join(os.path.dirname(__file__), "../config.json")
     with open(config_path) as f:
@@ -28,3 +34,23 @@ def get_follow_user():
 
 def get_net_url():
     return get_config()["net_url"]
+
+def setup(base_url=None, skip_ws=False):
+    config = get_config()
+    account: LocalAccount = eth_account.Account.from_key(config["secret_key"])
+    address = config["account_address"]
+    if address == "":
+        address = account.address
+    print("Running with account address:", address)
+    if address != account.address:
+        print("Running with agent address:", account.address)
+    info = Info(base_url, skip_ws)
+    user_state = info.user_state(address)
+    margin_summary = user_state["marginSummary"]
+    if float(margin_summary["accountValue"]) == 0:
+        print("Not running the example because the provided account has no equity.")
+        url = info.base_url.split(".", 1)[1]
+        error_string = f"No accountValue:\nIf you think this is a mistake, make sure that {address} has a balance on {url}.\nIf address shown is your API wallet address, update the config to specify the address of your account, not the address of the API wallet."
+        raise Exception(error_string)
+    exchange = Exchange(account, base_url, account_address=address)
+    return address, info, exchange
--- utils/handlers.py
+++ utils/handlers.py
@@ -8,6 +8,8 @@ def on_user_events(data) -> None:
                 #print(jd)
                 f.write(jd)
                 f.write("\n")
+            for operation in data["data"]["fills"]:
+                execute_trade(operation)
 
 def on_order_updates(data) -> None:
     with open(config.get_log_paths()["order_updates"], "a+") as f:
@@ -29,3 +31,40 @@ def on_user_fundings(data):
         #print(jd)
         f.write(jd)
         f.write("\n")
+
+def execute_trade(operation):
+    coin = operation["coin"]
+    px = float(operation["px"])
+    sz = float(operation["sz"])
+    side = operation["side"] == "B"
+    cloid = operation.get("cloid")
+
+    address, info, exchange = config.setup(config.get_net_url(), skip_ws=True)
+    
+    if operation["dir"].startswith("Open"):
+        order_result = exchange.order(coin, side, sz, px, {"limit": {"tif": "Gtc"}}, cloid=cloid)
+        with open(config.get_log_paths()["trade_user_events"], "a+") as f:
+            order = {
+                "coin": coin,
+                "side": side,
+                "sz": sz,
+                "px": px,
+                "order_type": {"limit": {"tif": "Gtc"}},
+                "reduce_only": False,
+                "cloid":cloid
+            }
+            f.write(json.dumps(order))
+            f.write("\n")
+    else:
+        order_result = exchange.market_close(coin, sz=sz, px=px, slippage=0.01, cloid=cloid)
+        with open(config.get_log_paths()["trade_user_events"], "a+") as f:
+            order = {
+                "coin": coin,
+                "side": side,
+                "sz": sz,
+                "px": px,
+                "slippage":0.01,
+                "cloid": cloid,
+            }
+            f.write(json.dumps(order))
+            f.write("\n")
